f77e724504277b6796d8ca4316419b263716b8cb
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-23 17:37:11 +0300

    Implemented the Subscribe and UnSubscribe

---
+++ DolphinFacebook/DolphinFacebook/DolphinsFacebookClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MamaFacebook.Abstract;
+
+namespace DolphinFacebook
+{
+    public class DolphinsFacebookClient : IFacebookClient
+    {
+        public event Action<string> NewWallPost;
+
+        public void Subscribe(IFacebookClient publisher)
+        {
+            NewWallPost += publisher.WriteNewWallPost;
+        }
+
+        public void Unsubscribe(IFacebookClient publisher)
+        {
+            NewWallPost -= publisher.WriteNewWallPost;
+        }
+
+        public void WriteNewWallPost(string wallPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
